76224c590a0154c3e598973087d38c89d5a4112e
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-06-22 21:44:19 -0500

    changed the err usestate  to use a boolean value instead of an error message string

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,15 @@ import Values from "values.js";
 
 function App() {
   const [color, setColor] = useState("");
-  const [err, setErr] = useState("");
+  const [err, setErr] = useState(false);
   const [list, setList] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     try {
       let colors = new Values(color).all(10);
-      console.log(colors);
     } catch (error) {
-      setErr("Invalid color format. Please provide a valid color.");
-      console.error(error);
+      setErr(true);
     }
   };
   return (
@@ -37,7 +35,11 @@ function App() {
         </form>
       </section>
       <section className="color">
-        {err && <p className="error-message">{err}</p>}
+        {err && (
+          <p className="error-message">
+            Invalid color format. Please provide a valid color.
+          </p>
+        )}
         <h4>list goes here</h4>
       </section>
     </>
